33fb01e0a33c0c2d5d6d7ab25d87f992d79ab486
amilcar <52044033+Terminal-Redux@users.noreply.github.com>
2022-05-23 21:51:03 -0500

    Final de seccion 8

---
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-region',
+  templateUrl: './por-region.component.html',
+  styles: [
+  ]
+})
+export class PorRegionComponent {
+
+  constructor() { }
+
+}
